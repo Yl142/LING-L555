e37dc9802fd72f075886ddd8f3af35455cf2affe
yuxuan <yl142@iu.edu>
2021-12-15 23:32:11 -0500

    update

---
+++ practicals/practical02/gloss.py
@@ -0,0 +1,10 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('isl-ex.xml')
+root = tree.getroot()
+
+print(root.tag)
+for tier in root.findall('.//tier'):
+        if tier.attrib['id'] == 'n':
+                for item in tier.findall('.//item'):
+                        print(item.text)
